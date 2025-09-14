d76e972249cce17aa16e486d289c69292567de36
volodyaKNU <voloda.m@knu.ua>
2025-09-14 16:14:59 +0300

    Add project files.

---
+++ Lab1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using Lab1.logic;
+using ScottPlot;
+using ScottPlot.WinForms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        // 1) Поле для графіка, щоб мати до нього доступ у будь-якому обробнику
+
+        private FormsPlot plot; // контрол для графіка
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitPlot();
+
+            this.Load += (_, __) =>
+            {
+                double[] xs = Enumerable.Range(0, 200).Select(i => i / 20.0).ToArray();
+                double[] ys = xs.Select(x => Math.Sin(x)).ToArray();
+                plot.Plot.Clear();
+                plot.Plot.Add.Scatter(xs, ys);
+                plot.Refresh();
+            };
+        }
+
+        private void InitPlot()
+        {
+            plot = new FormsPlot
+            {
+                Name = "formsPlot1",
+                Dock = DockStyle.Bottom,   // прикріпимо до низу форми
+                Height = 380               // фіксована висота, щоб не накривався іншими контролами
+            };
+            Controls.Add(plot);
+            plot.BringToFront();           // на випадок, якщо DataGridView закриває його
+        }
+
+        // 3) Створюємо й розміщуємо Chart на формі
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+
+       private void button1_Click(object sender, EventArgs e)
+{
+    var tabul = new Tabul();
+
+    double xn = Convert.ToDouble(textBox1.Text);
+    double xk = Convert.ToDouble(textBox2.Text);
+    double h  = Convert.ToDouble(textBox3.Text);
+    double a  = Convert.ToDouble(textBox4.Text);
+
+    dataGridView1.Rows.Clear();
+    tabul.tab(xn, xk, h, a);
+
+    // таблиця + підготовка масивів для графіка
+    double[] xs = new double[tabul.n];
+    double[] ys = new double[tabul.n];
+
+    for (int i = 0; i < tabul.n; i++)
+    {
+        double x = Math.Round(tabul.xy[i, 0], 2);
+        double y = Math.Round(tabul.xy[i, 1], 3);
+
+        dataGridView1.Rows.Add(x, y);
+
+        xs[i] = x;
+        ys[i] = y;
+    }
+
+    // ⬇️ побудова графіка ScottPlot
+    plot.Plot.Clear();
+    plot.Plot.Add.Scatter(xs, ys);   // лінія
+    // необов'язково: точки поверх лінії
+    // plot.Plot.Add.Markers(xs, ys, size: 4);
+
+    plot.Refresh();
+}
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab1/logic/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.logic
+{
+
+    class Tabul
+    {
+        public double[,] xy = new double[1000, 2];
+        public int n = 0;
+
+        private const double EPS = 1e-9;
+
+        private static bool TanUndefined(double angle)
+        {
+         return   Math.Abs(Math.Cos(angle)) < 1e-12;
+        }
+
+        private double f1(double x)
+        {
+            if (TanUndefined(x + 1)) return double.NaN;          // полюси тангенса
+            double den = x * x * x * x + 2 * x * x * x - x;      // x(x+1)(x^2+x−1)
+            if (Math.Abs(den) < EPS) return double.NaN;          // ділення на нуль
+            double t = Math.Tan(x + 1);
+            return (t * t) / den;
+        }
+
+        private double f2(double x)
+        {
+            double ang = 2 * x - 1;
+            if (TanUndefined(ang)) return double.NaN;            // полюси тангенса
+            double den = Math.Tan(ang) + 1.0;
+            if (Math.Abs(den) < EPS) return double.NaN;          // tg(2x−1) = −1
+            return (2 * x + 2) / den;
+        }
+
+        private double f3(double x)
+        {
+            double num = Math.Cos(x + 2);
+            num *= num;                                          // cos^2
+            double den = Math.Exp(-2.0 * x) + Math.Pow(3.0 * x * x, 0.25) + 1.0;
+            return num / den;                                    // знаменник завжди > 1
+        }
+
+        private double F(double x, double a)
+        {
+            if (x <= 0) return f1(x);
+            if (x <= a) return f2(x);
+            return f3(x);
+        }
+
+        public void tab(double xn, double xk, double h, double a)
+        {
+            double x = xn, y;
+            int i = 0;
+            while (x <= xk)
+            {
+                y = F(x, a);
+                xy[i, 0] = x;
+                xy[i, 1] = y;
+                x += h;
+                i++;
+            }
+            n = i;
+        }
+    }
+}
